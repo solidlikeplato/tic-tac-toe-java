c547c87703d08ebc50251d955268ab1cc69e533e
Ethan Whaley <51497042+solidlikeplato@users.noreply.github.com>
2020-02-18 16:00:47 -0600

    modified app to take input/output streams, modified app test to utilize these for testing... still need to clean up quite a bit

--- src/main/java/tic/tac/toe/App.java
+++ src/main/java/tic/tac/toe/App.java
@@ -1,20 +1,29 @@
 package tic.tac.toe;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class App {
-  public String getGreeting() {
-    return new Game().getGreeting();
+
+  public App() {
+    System.setIn(System.in);
+    System.setOut(System.out);
   }
 
+  public App(InputStream in, ByteArrayOutputStream out) {
+    System.setIn(in);
+    System.setOut(new PrintStream(out));
+  }
   public static void main(String[] args) {
     Game game = new Game();
-    System.out.println(game.getGreeting());
-    System.out.println(game.displayGame());
-    System.out.println(game.prompt());
+    // System.out.println(game.getGreeting());
+    // System.out.println(game.displayGame());
+    // System.out.println(game.prompt());
     Scanner sc = new Scanner(System.in);
     int square = sc.nextInt();
-    sc.close();
     game.addMark(square);
     System.out.println(game.displayGame());
+    sc.close();
   }
 }
--- src/test/java/tic/tac/toe/AppTest.java
+++ src/test/java/tic/tac/toe/AppTest.java
@@ -1,12 +1,41 @@
 package tic.tac.toe;
 
-import org.junit.Test;
+import org.junit.*;
 import static org.junit.Assert.*;
+import java.io.*;
 
 public class AppTest {
+  private final InputStream systemIn = System.in;
+  private final PrintStream systemOut = System.out;
+
+  private ByteArrayInputStream testIn;
+  private ByteArrayOutputStream testOut;
+
+  @Before
+  public void setUpOutput() {
+    testOut = new ByteArrayOutputStream();
+  }
+
+  private void provideInput(String data) {
+    testIn = new ByteArrayInputStream(data.getBytes());
+  }
+
+  private String getOutput() {
+    return testOut.toString();
+  }
+
+  @After
+  public void restoreSystemInputOutput() {
+    System.setIn(systemIn);
+    System.setOut(systemOut);
+  }
+
   @Test
-  public void testAppHasAGreeting() {
-    App app = new App();
-    assertNotNull("app should have a greeting", app.getGreeting());
+  public void testTakesUserInput() {
+    final String UserInput = "5";
+    provideInput(UserInput);
+    App app = new App(testIn, testOut);
+    app.main(new String[0]);
+    assertEquals("Should put symbol in correct place", "   |   |   \n---|---|---\n   | X |   \n---|---|---\n   |   |   \n", getOutput());
   }
 }
