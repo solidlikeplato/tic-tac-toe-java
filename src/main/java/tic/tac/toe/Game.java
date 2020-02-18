d0ef12e54e8f88853aaa95a341b95706c840eea5
Ethan Whaley <51497042+solidlikeplato@users.noreply.github.com>
2020-02-18 11:29:18 -0600

    removed further testing / code to limit this branch to only displaying empty board

--- src/main/java/tic/tac/toe/Game.java
+++ src/main/java/tic/tac/toe/Game.java
@@ -7,43 +7,6 @@ public class Game {
     this.board = new char[9];
     Arrays.fill(board, ' ');
   }
-
-  public char winner() {
-    int[][] winningMoves = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
-    char winningPlayer = ' ';
-    for (int i = 0; i < winningMoves.length; i++){
-      int[] threeToCheck = winningMoves[i];
-
-      char[] moves = { board[threeToCheck[0]], board[threeToCheck[1]], board[threeToCheck[2]] };
-      
-      if (moves[0] == moves[1] && moves[1] == moves[2]) {
-        winningPlayer = moves[0];
-      }
-    }
-    return winningPlayer;
-  }
-  public boolean isBoardFull() {
-    boolean boardFull = true;
-    int i = 0;
-    while (boardFull && i < board.length) {
-      if (board[i] == ' ') {
-        boardFull = false;
-      }
-      i++;
-    }
-    return boardFull;
-  }
-
-  public boolean gameOver() {
-    
-    boolean isWinner = winner() == 'X' || winner() == 'O';
-    
-    return isBoardFull() || isWinner;
-  }
-
-  public Game(char[] newBoard) {
-    this.board = newBoard;
-  }
   
   public String displayGame() {
     String firstRow = " " + board[0] + " | " + board[1] + " | "+ board[2]+ " \n";
--- src/test/java/tic/tac/toe/GameTest.java
+++ src/test/java/tic/tac/toe/GameTest.java
@@ -15,32 +15,5 @@ public class GameTest {
     Game game = new Game();
     assertEquals("game should have an empty board", game.displayGame(), "   |   |   \n---|---|---\n   |   |   \n---|---|---\n   |   |   ");
   }
-
-  @Test
-  public void testFullBoardDisplay() {
-    char x = 'X';
-    char o = 'O';
-    char[] board = new char[] { x,o,x,x,o,x,o,x,o };
-    Game game = new Game(board);
-    assertEquals("full board should display correctly", game.displayGame(), " X | O | X \n---|---|---\n X | O | X \n---|---|---\n O | X | O ");
-  }
-
-  @Test
-  public void testGameDoneBoardFull() {
-    char x = 'X';
-    char o = 'O';
-    char[] board = new char[] { x,o,x,x,o,x,o,x,o };
-    Game game = new Game(board);
-    assertTrue("full board should lead to game over condition", game.gameOver());
-  }
-
-  @Test
-  public void testGameDoneWinningBoard() {
-    char x = 'X';
-    char o = 'O';
-    char[] board = new char[] { x,x,x,o,' ',o,o,' ',' ' };
-    Game game = new Game(board);
-    assertTrue("winning board should lead to game over condition", game.gameOver());
-  }
 }
 
